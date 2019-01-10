dda56cb2d80595f66fa5eb252f2911e1a09dd491
Michael Pitzer <mail@mpitzer.at>
2019-01-11 00:38:12 +0100

    Remove bower support

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,22 +11,6 @@ module.exports = function() {
     return {
       scenarios: [
         {
-          name: 'ember-lts-2.8',
-          bower: {
-            dependencies: {
-              'ember': 'components/ember#lts-2-8'
-            },
-            resolutions: {
-              'ember': 'lts-2-8'
-            }
-          },
-          npm: {
-            devDependencies: {
-              'ember-source': null
-            }
-          }
-        },
-        {
           name: 'ember-lts-2.12',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
